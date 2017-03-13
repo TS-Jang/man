469b9f6f6d4ab36a63fbf6bca25505c828eafc1e
git@github.com:TS-Jang/neogulman.git <my@user>
2017-03-13 20:33:41 +0900

    test

---
+++ test/src/main/java/com/example/TestApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+		
+		
+		System.out.println("asdfas");
+		
+		
+	}
+}
